15e31e5083acc8bfe11b14fbdf0bb5bbbda85647
AgrimSrivastava <agrimsrivastavadk@gmail.com>
2019-01-14 22:49:50 +0530

    FirstCommit

---
+++ app/src/main/java/com/theagrim/flashchatnewfirebase/InstantMessage.java
@@ -0,0 +1,26 @@
+package com.theagrim.flashchatnewfirebase;
+
+class InstantMessage {
+
+    private String message;
+    private String author;
+
+    InstantMessage(String message, String author) {
+        this.message = message;
+        this.author = author;
+    }
+
+    public InstantMessage() {
+
+
+
+    }
+
+    String getMessage() {
+        return message;
+    }
+
+    String getAuthor() {
+        return author;
+    }
+}
